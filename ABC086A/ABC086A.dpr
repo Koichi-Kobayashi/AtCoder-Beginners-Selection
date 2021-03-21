332b56896138a005edf2a83d74ff17be25c148ee
Koichi-Kobayashi <koichi.code@gmail.com>
2021-03-21 14:22:16 +0900

    プロジェクトの整理と2問目の追加

---
+++ ABC086A/ABC086A.dpr
@@ -0,0 +1,18 @@
+﻿program PracticeA;
+
+var
+  a, b, c : Integer;
+  s : ShortString;
+
+begin
+  // 整数の入力
+  Readln(a);
+  // スペース区切りの整数の入力
+  Read(b);
+  Readln(c);
+  // 文字列の入力
+  Readln(s);
+  // 出力
+  Writeln(a + b + c, ' ', s);
+end.
+
