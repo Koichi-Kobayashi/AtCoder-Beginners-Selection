4299e9218cb5207af3402107c484e4d4b7f08506
Koichi-Kobayashi <koichi.code@gmail.com>
2021-03-23 22:32:51 +0900

    Placing Marbles

---
+++ ABC081A/ABC081A.dpr
@@ -0,0 +1,18 @@
+﻿program ABC081A;
+
+uses sysutils;
+
+var
+  a : string;
+  I, sum : Integer;
+
+begin
+  sum := 0;
+  Readln(a);
+  for I := 0 to Length(a) - 1 do begin
+    if StrToInt(Copy(a, I, 1)) = 1 then
+      sum := sum + StrToInt(Copy(a, I, 1));
+  end;
+  Writeln(sum);
+end.
+
