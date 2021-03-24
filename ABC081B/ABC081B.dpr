721c47b766dbf5298add16e01d29f1679cd2aeee
Koichi-Kobayashi <koichi.code@gmail.com>
2021-03-24 23:48:37 +0900

    Shift onlyとりあえず

---
+++ ABC081B/ABC081B.dpr
@@ -0,0 +1,26 @@
+﻿program ABC081B;
+
+uses sysutils, System.Generics.Collections;
+
+type
+  TArrayOfInteger = array of integer;
+
+var
+  a, b : integer;
+  List: TList<Integer>;
+  sum : Integer = 0;
+  I : Integer;
+
+  function dev() : array[]:
+
+begin
+  Readln(a);
+  Readln(b);
+
+  for I := 0 to a - 1 do begin
+    if StrToInt(Copy(a, I, 1)) = 1 then
+      Inc(sum);
+  end;
+  Writeln(sum);
+end.
+
